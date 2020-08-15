03da641362874df18a0174f7430322c7dfe2d581
KauanRakoski <kauan.k.rakoski@gmail.com>
2020-08-15 14:23:42 -0300

    credentials exposed

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,14 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-    apiKey: "...",
-    authDomain: "...",
-    databaseURL: "...",
-    projectId: "...",
-    storageBucket: "...",
-    messagingSenderId: "...",
-    appId: "...",
-    measurementId: "..."
+    apiKey: "AIzaSyCFLF493Rxuh5B-S_RNWIfe6LHi_XrGtnM",
+    authDomain: "hostimage-8f942.firebaseapp.com",
+    databaseURL: "https://hostimage-8f942.firebaseio.com",
+    projectId: "hostimage-8f942",
+    storageBucket: "hostimage-8f942.appspot.com",
+    messagingSenderId: "656704638697",
+    appId: "1:656704638697:web:4fbbdd77b80c87378f6a78",
+    measurementId: "G-RQD141BX5L"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
